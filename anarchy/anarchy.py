986a0990f78af549f1e26fca69fcb1d258c9f347
Robbie <42142350+robbai@users.noreply.github.com>
2019-04-06 20:39:57 +0000

    Giving Matrix3D a new home (#114)

--- anarchy/anarchy.py
+++ anarchy/anarchy.py
@@ -191,20 +191,6 @@ def dodge(self, angle_to_ball: float, target=None):
             self.dodging = False
 
 
-class Matrix3D:
-    def __init__(self,r):
-        CR = math.cos(r[2])
-        SR = math.sin(r[2])
-        CP = math.cos(r[0])
-        SP = math.sin(r[0])
-        CY = math.cos(r[1])
-        SY = math.sin(r[1])
-        self.data = [Vector3(CP*CY, CP*SY, SP),Vector3(CY*SP*SR-CR*SY, SY*SP*SR+CR*CY, -CP * SR),Vector3(-CR*CY*SP-SR*SY, -CR*SY*SP+SR*CY, CP*CR)]
-
-    def dot(self,vector):
-        return Vector3(self.data[0].dot(vector),self.data[1].dot(vector),self.data[2].dot(vector))
-
-
 def halfflip(self):
     if not self.halfflipping and self.car.has_wheel_contact:
         self.halfflipping = True
