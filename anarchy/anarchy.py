e6c87494e9cdf2a7610bfadcece2015d3f47277b
IamEld3st <iameld3st@gmail.com>
2020-01-21 13:42:37 +0100

    OOPsIE I Dont LIke TABz (#177)

--- anarchy/anarchy.py
+++ anarchy/anarchy.py
@@ -58,7 +58,7 @@ class Anarchy(BaseAgent):
         self.steer_correction_radians: float = 0
         self.demo: Optional[Demolition] = None
         self.gamemode: Gamemode = Gamemode.SOCCAR
-	self.score = 0
+        self.score = 0
 
     def load_config(self, config_header):
         render_statue = config_header.getboolean("render_statue")
@@ -93,7 +93,7 @@ class Anarchy(BaseAgent):
         correct_side_of_ball: bool = ((impact_projection.y - car_location.y) * team_sign > 0)
         # Hi robbie!
 
-	# RUSH B
+	    # RUSH B
         currentScore = 0
         for i in range(packet.num_teams):
             currentScore += packet.teams[i].score
