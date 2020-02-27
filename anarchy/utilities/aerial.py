5ae71cb65cc6bb902d8f2697b338ac76153a24b6
Blocks <21197843+TheBlocks@users.noreply.github.com>
2020-02-27 14:46:15 +0100

    Add Black formatting for aerial.py (#202)

--- anarchy/utilities/aerial.py
+++ anarchy/utilities/aerial.py
@@ -20,21 +20,33 @@ class Info:
         self.game_time = packet.game_info.seconds_elapsed
         self.car = packet.game_cars[index]
         self.car_location = Vector3(
-            self.car.physics.location.x, self.car.physics.location.y, self.car.physics.location.z
+            self.car.physics.location.x,
+            self.car.physics.location.y,
+            self.car.physics.location.z,
         )
         self.car_velocity = Vector3(
-            self.car.physics.velocity.x, self.car.physics.velocity.y, self.car.physics.velocity.z
+            self.car.physics.velocity.x,
+            self.car.physics.velocity.y,
+            self.car.physics.velocity.z,
         )
         self.car_matrix = Matrix3D(
-            [self.car.physics.rotation.pitch, self.car.physics.rotation.yaw, self.car.physics.rotation.roll]
+            [
+                self.car.physics.rotation.pitch,
+                self.car.physics.rotation.yaw,
+                self.car.physics.rotation.roll,
+            ]
         )
         self.rotation_velocity = self.car_matrix.dot(self.car.physics.angular_velocity)
         self.ball = packet.game_ball
         self.ball_location = Vector3(
-            self.ball.physics.location.x, self.ball.physics.location.y, self.ball.physics.location.z
+            self.ball.physics.location.x,
+            self.ball.physics.location.y,
+            self.ball.physics.location.z,
         )
         self.ball_velocity = Vector3(
-            self.ball.physics.velocity.x, self.ball.physics.velocity.y, self.ball.physics.velocity.z
+            self.ball.physics.velocity.x,
+            self.ball.physics.velocity.y,
+            self.ball.physics.velocity.z,
         )
 
 
@@ -50,7 +62,9 @@ def default_pd(
     yaw = steer_pd(e1, -info.rotation_velocity.z / 6)
     e2 = math.atan2(local.z, local.x)
     pitch = steer_pd(e2, info.rotation_velocity.y / 6)
-    roll = steer_pd(-info.car.physics.rotation.roll, info.rotation_velocity.x / 6)  # keeps the bot upright
+    roll = steer_pd(
+        -info.car.physics.rotation.roll, info.rotation_velocity.x / 6
+    )  # keeps the bot upright
     if error == False:
         return steer, yaw, pitch, roll
     else:
@@ -85,7 +99,11 @@ def future(
 
 
 def backsolve_future(
-    location: Vector3, velocity: Vector3, future: Vector3, time: float, radius: float = 92.75
+    location: Vector3,
+    velocity: Vector3,
+    future: Vector3,
+    time: float,
+    radius: float = 92.75,
 ) -> Vector3:  # finds acceleration needed to arrive at a future given a location and time
     d = future - location
     d -= d.normalized * radius
@@ -106,22 +124,61 @@ class aerial_option_b:  # call at your own risk: yeets towards ball after taking
         self.jt = game_time_started
         self.target: Vector3 = None
 
-    def execute(self, packet: GameTickPacket, index, ball_prediction: BallPrediction = None) -> SimpleControllerState:
+    def execute(
+        self, packet: GameTickPacket, index, ball_prediction: BallPrediction = None
+    ) -> SimpleControllerState:
         info = Info(packet, index)
 
-        if self.time == -9:  # if we don't have a target time, guess one using really bad math
+        if (
+            self.time == -9
+        ):  # if we don't have a target time, guess one using really bad math
             eta = math.sqrt(((info.ball_location - info.car_location).length) / 529.165)
             targetetaloc = future(info.ball_location, info.ball_velocity, eta)
-            before = abs(dpp3D(info.ball_location, info.ball_velocity, info.car_location, info.car_velocity))
-            after = abs(dpp3D(targetetaloc, info.ball_velocity, info.car_location, info.car_velocity))
+            before = abs(
+                dpp3D(
+                    info.ball_location,
+                    info.ball_velocity,
+                    info.car_location,
+                    info.car_velocity,
+                )
+            )
+            after = abs(
+                dpp3D(
+                    targetetaloc,
+                    info.ball_velocity,
+                    info.car_location,
+                    info.car_velocity,
+                )
+            )
             if sign(before) == sign(after):  # sign returns 1 or -1
-                eta = math.sqrt(clamp(((info.ball_location - info.car_location).length + before) / 529.165, 0.01, 9999))
+                eta = math.sqrt(
+                    clamp(
+                        ((info.ball_location - info.car_location).length + before)
+                        / 529.165,
+                        0.01,
+                        9999,
+                    )
+                )
             else:
                 eta = math.sqrt(
-                    clamp(((info.ball_location - info.car_location).length + before + after) / 529.165, 0.01, 9999)
+                    clamp(
+                        (
+                            (info.ball_location - info.car_location).length
+                            + before
+                            + after
+                        )
+                        / 529.165,
+                        0.01,
+                        9999,
+                    )
                 )
-            test = dpp3D(targetetaloc, info.ball_velocity, info.car_location, info.car_velocity)
-            eta = math.sqrt(((info.ball_location - info.car_location).length + max(0, test)) / 529.165)
+            test = dpp3D(
+                targetetaloc, info.ball_velocity, info.car_location, info.car_velocity
+            )
+            eta = math.sqrt(
+                ((info.ball_location - info.car_location).length + max(0, test))
+                / 529.165
+            )
             self.time = info.game_time + eta
             target = Vector3(0, 0, 0)
         else:
@@ -129,13 +186,21 @@ class aerial_option_b:  # call at your own risk: yeets towards ball after taking
                 self.time - info.game_time, -2.0, 10.0
             )  # agent will continue aerial up to 2 seconds after predicted intercept time
             if time_remain != 0:
-                target = future(info.ball_location, info.ball_velocity, time_remain, ball_prediction)
+                target = future(
+                    info.ball_location, info.ball_velocity, time_remain, ball_prediction
+                )
             else:
                 time_remain = 0.1
-                target = future(info.ball_location, info.ball_velocity, 0.1, ball_prediction)
+                target = future(
+                    info.ball_location, info.ball_velocity, 0.1, ball_prediction
+                )
 
             # Shooting offset
-            enemy_goal = Vector3(0, (1 if info.car.team == 0 else -1) * 5120, clamp(target.z / 2, 100, 600))
+            enemy_goal = Vector3(
+                0,
+                (1 if info.car.team == 0 else -1) * 5120,
+                clamp(target.z / 2, 100, 600),
+            )
             target += (target - enemy_goal).normalized * 100
 
             """
@@ -144,7 +209,9 @@ class aerial_option_b:  # call at your own risk: yeets towards ball after taking
             else:
                 target = info.car_velocity
             """
-            target = backsolve_future(info.car_location, info.car_velocity, target, time_remain, radius=0)
+            target = backsolve_future(
+                info.car_location, info.car_velocity, target, time_remain, radius=0
+            )
         controller, self.jt = deltaC(info, target, self.jt)
         self.target = target
         return controller
@@ -172,7 +239,9 @@ def deltaC(
             c.jump = True
         elif tsj < 0.25:
             c.jump = False
-        elif tsj >= 0.25 and tsj < 0.27 and target.z > 560:  # considers a double-jump if we still need to go up a lot
+        elif (
+            tsj >= 0.25 and tsj < 0.27 and target.z > 560
+        ):  # considers a double-jump if we still need to go up a lot
             c.jump = True
             c.boost = False
             c.yaw = c.pitch = c.roll = 0
